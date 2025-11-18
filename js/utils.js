1d30733e29f9e0102d6a7359304a3b34a926b1ae
masekaku <aguslistiyanto27@gmail.com>
2025-11-19 01:41:44 +0700

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,7 @@
 // Ambil video ID dari path: /f/:id
 export function getVideoIdFromPath() {
   const pathParts = window.location.pathname.split('/');
-  return parseInt(pathParts[2]) || 1; // default 1
+  return parseInt(pathParts[2]) || 1;
 }
 
 // Load JSON daftar video
@@ -9,7 +9,7 @@ export async function loadVideoList(jsonPath = 'videos.json') {
   try {
     const res = await fetch(jsonPath);
     return await res.json();
-  } catch (err) {
+  } catch(err) {
     console.error('Failed to load videos.json:', err);
     return [];
   }
